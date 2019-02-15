63eb32a39ade9db65be4aba2e43869a0978f35be
Kenton Archer <ken10archer@gmail.com>
2019-02-15 23:50:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,10 @@ function updatePlaylist(playlist,artists,song)
   return playlist  
 } 
 
+function removeFromPlaylist(playlist,artists)
+{
+  return 
+  object.assign({}, playlist, { 
+    artists:["song"]
+  })
+}
